66e446fa84cf413ea1be935e368337137832bda6
Haoxuan Deng <1143313215@qq.com>
2021-03-22 18:00:51 +0000

    Commit notes

--- first_project_test/main.c
+++ first_project_test/main.c
@@ -11,10 +11,16 @@
 #include "spi_comm.h"
 #include "sensors/proximity.h"
 #include "motors.h"
+#include "sensors/VL53L0X/VL53L0X.h"
+#include "chprintf.h"
+#include "usbcfg.h"
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
 
-
-
-
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
 
 int main(void)
 {
@@ -22,20 +28,22 @@ int main(void)
     halInit();
     chSysInit();
     mpu_init();
-
-    // Initiate and turn off all eight ring LEDs
+    // Initiate the USB communication peripheral
     clear_leds();
     spi_comm_start();
-
+    // Initiate the internal process communication bus
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    //
+    proximity_start();
+    calibrate_ir();
     // Initiate the motors
     motors_init();
-
-    //messagebus_tbus;
-    //MUTEX_DECL(bus_lock);
-    //CONDVAR_DECL(bus_condvar);
-    //
-    //// Initiate the inter processcommunication bus
-    //messagebus_init(&bus, &bus_lock, &bus_condvar);
+    // Initiate the distance sensor module
+    VL53L0X_start();
+    // Initiate the USB communication peripheral
+    usb_start();
+    // Initialise the UART1 channel
+    serial_start();
 
     /* Infinite loop. */
     while (1) {
@@ -46,11 +54,27 @@ int main(void)
     	//waits 1 second
     	chThdSleepMilliseconds(1000);
 
-    	// Motor setting for running
+    	// Motor setting
     	left_motor_set_speed(-420);
     	right_motor_set_speed(420);
     	//waits 1 second
         chThdSleepMilliseconds(1000);
+
+        // Read proximity value
+        for (int i = 0; i <= 7; i++){
+        	double proximity_value = get_calibrated_prox(i);
+        	if (SDU1.config->usbp->state==USB_ACTIVE) {
+        		chprintf((BaseSequentialStream*)&SDU1, "sensor %d: %2d\n", i, proximity_value);
+        	}
+        }
+        chprintf((BaseSequentialStream*)&SDU1, "\n");
+        //waits 1 second
+        chThdSleepMilliseconds(1000);
+
+//        // Read the distance
+//        double dist = VL53L0X_get_dist_mm();
+//        // Skip printing if port not opened.
+//
     }
 }
 
